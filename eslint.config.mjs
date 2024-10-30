50857fc5ce28a56a10ccdfd41590b32f9347e8fc
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-10-30 18:39:02 +0330

    (chore): Add all the needed dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,8 @@
-// import globals from "globals";
-// import react from "eslint-plugin-react";
-// import next from "@next/eslint-plugin-next";
-// import typescriptEslint from "@typescript-eslint/eslint-plugin";
-// import typescriptParser from "@typescript-eslint/parser";
-// import reactRecommended from "eslint-plugin-react/configs/recommended.js";
-
-// export default [
-//   {
-//     ignores: [".husky/**/*", ".next/**/*", "node_modules/**/*", ".vscode/**/*"],
-//   },
-//   {
-//     files: ["**/*.{ts,tsx}"],
-//     ...reactRecommended,
-//     settings: {
-//       version: "detect",
-//     },
-//     languageOptions: {
-//       ...reactRecommended.languageOptions,
-//       ecmaVersion: "latest",
-//       sourceType: "module",
-//       parser: typescriptParser,
-//       parserOptions: {
-//         ecmaFeatures: {
-//           jsx: true,
-//         },
-//       },
-//       globals: {
-//         ...globals.serviceworker,
-//         ...globals.browser,
-//       },
-//     },
-//     plugins: {
-//       "@typescript-eslint": typescriptEslint,
-//       react,
-//       next,
-//     },
-//     rules: {
-//       "react/jsx-no-unused-vars": "off",
-//       "@typescript-eslint/no-unused-vars": ["error"],
-//       "react/react-in-jsx-scope": 0,
-//       "spaced-comment": ["error", "always", { markers: ["/"] }],
-//       "new-cap": 0,
-//     },
-//   },
-// ];
-
 import globals from "globals";
 import react from "eslint-plugin-react";
-import next from "@next/eslint-plugin-next"; // Correctly import Next.js plugin
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 
 export default [
   {
@@ -57,7 +10,12 @@ export default [
   },
   {
     files: ["**/*.{ts,tsx}"],
+    ...reactRecommended,
+    settings: {
+      version: "detect",
+    },
     languageOptions: {
+      ...reactRecommended.languageOptions,
       ecmaVersion: "latest",
       sourceType: "module",
       parser: typescriptParser,
@@ -74,7 +32,6 @@ export default [
     plugins: {
       "@typescript-eslint": typescriptEslint,
       react,
-      next,
     },
     rules: {
       "react/jsx-no-unused-vars": "off",
@@ -82,10 +39,6 @@ export default [
       "react/react-in-jsx-scope": 0,
       "spaced-comment": ["error", "always", { markers: ["/"] }],
       "new-cap": 0,
-      // Include Next.js recommended rules directly
-      "next/no-html-link-for-pages": "error", // Example rule
-      "next/no-img-element": "warn", // Example rule
-      // Add other Next.js rules as necessary
     },
   },
 ];
